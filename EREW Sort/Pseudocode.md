a3edac5bf19731f8aaa59f5f36e2c55cb8a59c5d
Uri Martínez <39018208+UriMtzF@users.noreply.github.com>
2024-04-11 00:25:12 +0000

    Fixed with c++ EREW Sort

--- EREW Sort/Pseudocode.md
+++ EREW Sort/Pseudocode.md
@@ -2,7 +2,7 @@
 ```
 function mergeSort(a[n]) recursive
   if n >= 2 then
-    parallel call mergeSort(a[1:n/2] && a[n/2+1:n])
+    parallel call mergeSort(a[1:n/2] | a[n/2+1:n])
     call oddEvenMerge(a(n))
 
 function oddEvenMerge(a[n])
@@ -11,46 +11,11 @@ function oddEvenMerge(a[n])
       call interchange(a[0],a[1])
   else
     call oddEvenSplit(a[n], odd[n/2], even[n/2])
-    parallel call oddEvenMerge(odd[n/2] && even[n/2])
+    parallel call oddEvenMerge(odd[n/2] | even[n/2])
     for 0 <= i < n/2 do in parallel
       a[2i] = odd[i]
       a[2i+1] = even[i]
     for 0 <= i <= n/2 do in parallel
       if a[2i + 1] > a[2i] then
         call interchange(a[2i+1], a[2i])
-```
-
----
-
-```
-function oddEvenMergeSort(a[n], start, end) recursive
-  if start < end
-    middle = (start + end) / 2
-    do in parallel 
-      call oddEvenMergeSort(a[n], start, middle)
-      call oddEvenMergeSort(a[n], middle + 1, end)
-    parallelOddEvenMerge(a[n], start, end)
-
-function parallelOddEvenMerge(a[n], start, end) recursive
-  if end - start > 1
-    middle = (start + end) / 2
-    do in parallel
-      parallelOddEvenMerge(a[n], start, middle)
-      parallelOddEvenMerge(a[n], middle + 1, end)
-    do in parallel
-      parallelOddEvenMerge(a[n], start + 1, end)
-      parallelMerge(a[n], start, end)
-
-function parallelMerge(a[n], start, end)
-  middle = (start + end) / 2
-   for start <= i <= middle do in parallel
-    if a[i] > a[middle + 1]
-      temp = a[i]
-      a[i] = a[middle + 1]
-      a[middle + 1] = temp
-    for start + 1 <= i <= end - 1 (step by 2) do in parallel
-      if (a[i] > a[i+1])
-        temp = a[i]
-        a[i] = a[i+1]
-        a[i+1] = temp
 ```
---
+++ EREW Sort/erew-sort.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <omp.h>
+
+#ifdef __INTELLISENSE__
+#pragma diag_suppress 1696
+#endif
+
+void MergeSort(std::vector<int> &a);
+std::vector<int> OddEvenMerge(const std::vector<int> &left, const std::vector<int> &right);
+void Interchange(int &x, int &y);
+
+int main()
+{
+  std::vector<int> a = {9, 7, 5, 3, 15, 2, 4, 6, 8, 10};
+  MergeSort(a);
+  std::cout << "Arreglo ordenado:" << std::endl;
+  for (const auto &item : a)
+  {
+    std::cout << item << " ";
+  }
+  std::cout << std::endl;
+  return 0;
+}
+
+void MergeSort(std::vector<int> &a)
+{
+  int n = a.size();
+  if (n >= 2)
+  {
+    int mid = n / 2;
+    std::vector<int> left(a.begin(), a.begin() + mid);
+    std::vector<int> right(a.begin() + mid, a.end());
+
+#pragma omp parallel sections
+    {
+#pragma omp section
+      {
+        MergeSort(left);
+      }
+#pragma omp section
+      {
+        MergeSort(right);
+      }
+    }
+
+    std::vector<int> merged = OddEvenMerge(left, right);
+    std::copy(merged.begin(), merged.end(), a.begin());
+  }
+}
+
+std::vector<int> OddEvenMerge(const std::vector<int> &left, const std::vector<int> &right)
+{
+  int n = left.size() + right.size();
+  std::vector<int> result(n);
+
+  int i = 0, j = 0, k = 0;
+  while (i < left.size() && j < right.size())
+  {
+    if (left[i] <= right[j])
+    {
+      result[k++] = left[i++];
+    }
+    else
+    {
+      result[k++] = right[j++];
+    }
+  }
+  while (i < left.size())
+  {
+    result[k++] = left[i++];
+  }
+  while (j < right.size())
+  {
+    result[k++] = right[j++];
+  }
+
+#pragma omp parallel for
+  for (int m = 1; m < n - 1; m += 2)
+  {
+    if (result[m] > result[m + 1])
+    {
+      Interchange(result[m], result[m + 1]);
+    }
+  }
+
+  return result;
+}
+
+void Interchange(int &x, int &y)
+{
+  int temp = x;
+  x = y;
+  y = temp;
+}
